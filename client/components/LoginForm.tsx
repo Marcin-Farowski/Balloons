cf3bbf76276be0238434cbe2f4401a2cfcb77abc
Marcin Farowski <m.farowski@gmail.com>
2023-10-30 22:14:45 +0100

    Login page

---
+++ client/components/LoginForm.tsx
@@ -0,0 +1,26 @@
+import styles from "@styles/LoginForm.module.css";
+import React from "react";
+
+function LoginForm() {
+  return (
+    <div className={styles.formContainer}>
+      <h2 className={styles.loginText}>Login</h2>
+      <h3 className={styles.toStartAdventureText}>to start adventure</h3>
+      <input
+        type="text"
+        placeholder="Username"
+        className={styles.input + " " + styles.firstInput}
+      />
+      <input type="password" placeholder="Password" className={styles.input} />
+      <a href="#" className={styles.forgotPassword}>
+        Forgot password?
+      </a>
+      <button className={styles.loginButton}>Log in</button>
+      <div className={styles.signUp}>
+        Need an account? <a href="#">SIGN UP</a>
+      </div>
+    </div>
+  );
+}
+
+export default LoginForm;
