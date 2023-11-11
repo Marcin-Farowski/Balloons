969c2fa6cd7e95decdb4b10ec5878855f1e4b371
Patrycja Kalita <pati.kalita@gmail.com>
2023-11-11 21:16:29 +0100

    Frontend: Fixes

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -29,10 +29,7 @@ export default function RootLayout({
       className={`${plus_jakarta_sans.variable} ${cormorant.variable}`}
     >
       <body>
-        <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
-        <script noModule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
         {children}
-        
       </body>
     </html>
   );
